7efec077de19d6926b1e5d8546368ab3962da192
u826318 <62211195+u826318@users.noreply.github.com>
2025-03-22 21:33:11 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "simple-chatroom",
+  "version": "1.0.0",
+  "description": "A simple chat room using socket.io",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.5.1"
+  }
+}
